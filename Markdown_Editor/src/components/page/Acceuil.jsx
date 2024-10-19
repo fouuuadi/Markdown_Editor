c3ebf32173494f3dc2df78b8f99a03075e36615f
fouuuadi <fouadlamnaouar18@gmail.com>
2024-10-19 16:29:00 +0200

    feat: create router and add pages ('Accueil' and 'MarkdownUpdate'), ongoing API development

---
+++ Markdown_Editor/src/components/page/Acceuil.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+//render
+function Acceuil() {
+  return (
+    <div>
+      <h1>Hello Fouad</h1>
+      <h2>API</h2>
+    </div>
+  )
+}
+
+export default Acceuil
---
+++ Markdown_Editor/src/components/page/UpdateMarkdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UpdateMarkdown() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateMarkdown
