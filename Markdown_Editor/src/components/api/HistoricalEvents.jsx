38f7a07a7307e24200abea49f28a039532352d31
fouuuadi <fouadlamnaouar18@gmail.com>
2024-10-20 21:17:16 +0200

    feat: create component HistoricalEvents

---
+++ Markdown_Editor/src/components/api/HistoricalEvents.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//Evenement historiques qui ont eu lieu a la date du jour
+
+function HistoricalEvents() {
+
+  return (
+    <div>
+        <h1>Les évènements historiques qui ont eu lieu à la date du jour</h1>
+      
+    </div>
+  )
+}
+
+export default HistoricalEvents
--- Markdown_Editor/src/components/page/Acceuil.jsx
+++ Markdown_Editor/src/components/page/Acceuil.jsx
@@ -1,15 +1,17 @@
 import ShowTime from '../api/ShowTime'
 import QuoteOfTheDay from '../api/QuoteOfTheDay'
+import HistoricalEvents from '../api/HistoricalEvents'
 
 
 //render
 function Acceuil() {
   return (
     <div className='api'>
-      <h1>Hello Fouad</h1>
+      <h1>Markdown Editor</h1>
       <h2>API</h2>
       <ShowTime/>
       <QuoteOfTheDay/>
+      <HistoricalEvents/>
     </div>
   )
 }
