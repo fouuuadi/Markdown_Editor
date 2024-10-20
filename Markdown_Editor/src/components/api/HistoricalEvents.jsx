9b5796f655b90aad81d3aa0a4d3778193a6c23d6
fouuuadi <fouadlamnaouar18@gmail.com>
2024-10-20 22:24:35 +0200

    feat: complete HistoricalEvents API implementation

--- Markdown_Editor/src/components/api/HistoricalEvents.jsx
+++ Markdown_Editor/src/components/api/HistoricalEvents.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-
 //Evenement historiques qui ont eu lieu a la date du jour
 
+import { useEffect, useState } from "react"
+
 function HistoricalEvents() {
+    const [event, setEvent] = useState([]);
+    //On initialise avec un tableau vide car on attend les données de l'API
+
+    function fetchApiHistoricalEvents() {
+        fetch("https://history.muffinlabs.com/date")
+          .then((res) => res.json())
+          .then((result) => {
+            setEvent(result.data.Events); 
+            //console.log(result.data.Events);
+          })
+          .catch((err) => console.error(err));
+      }
+    //fetchApiHistoricalEvents(event)
+
+    useEffect(() => {
+        fetchApiHistoricalEvents();
+    }, [])
 
   return (
     <div>
         <h1>Les évènements historiques qui ont eu lieu à la date du jour</h1>
-      
+        {event.map((event, index) =>(
+            <div key={index}>
+                <h3>{event.year}</h3>
+                <p>{event.text}</p>
+            </div>
+        ))}
     </div>
   )
 }
